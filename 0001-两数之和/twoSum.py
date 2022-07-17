49dbc8c1228510a2f04bdddb210600fcd42bd68f
genpockzhang <zhangkangkai163@163.com>
2022-07-17 10:11:34 +0800

    增加解法二：哈希表

--- 0001-两数之和/twoSum.py
+++ 0001-两数之和/twoSum.py
@@ -1,4 +1,5 @@
 class Solution:
+    # 暴力解法
     def twoSum(self, nums, target):
         n = len(nums)
         for i in range(n):
@@ -6,5 +7,14 @@ class Solution:
                 if nums[i] + nums[j] == target:
                     return [i, j]
         
+        return []
+
+    # 哈希表
+    def twoSum_2(self, nums, target):
+        hashtable = {}
+        for i, num in enumerate(nums):
+            if target - num in hashtable:
+                return [hashtable[target-num], i]
+            hashtable[num] = i
         return []
 
